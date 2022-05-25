6bd8558a1dec2326cf8ee0706722ff0a5cfada37
GAEUN SHIN <newsilver1028@naver.com>
2022-05-25 11:10:03 +0900

    style: layout 적용

--- src/routes/dashboard/mediaChannel/index.tsx
+++ src/routes/dashboard/mediaChannel/index.tsx
@@ -1,5 +1,18 @@
+import WhiteSection from 'components/whiteSection';
+import MediaChannelGraph from './mediaChannelGraph';
+
+import styles from './mediaChannel.module.scss';
+
 const mediaChannel = () => {
-  return <div>mediaChannel</div>;
+  return (
+    <main>
+      <p className={styles.title}>매체 현황</p>
+      <WhiteSection>
+        <MediaChannelGraph />
+        {/* MediaChannelTable */}
+      </WhiteSection>
+    </main>
+  );
 };
 
 export default mediaChannel;
