336148f3e13a5fa89ef6324dd2064f2fa16d96f4
홍선영 <allie@hongseon-yeong-ui-MacBookPro.local>
2022-05-25 18:52:51 +0900

    refactor: mediaChannel의 recoilValue 타입추가

--- src/routes/dashboard/mediaChannel/index.tsx
+++ src/routes/dashboard/mediaChannel/index.tsx
@@ -7,9 +7,10 @@ import { fetchMediaChannelData } from 'services/fetchMediaChannelData';
 import { useQuery } from 'react-query';
 import { useRecoilValue } from 'recoil';
 import { periodState } from 'states';
+import { IPeriod } from 'types/period';
 
 const MediaChannel = () => {
-  const date = useRecoilValue(periodState);
+  const date = useRecoilValue<IPeriod>(periodState);
   const { isLoading, data } = useQuery(
     ['mediaChannelData', date],
     () => fetchMediaChannelData(date).then((res) => res.data),
