f91767e1a9c85de95304c04b4b6b1cb4d144bccd
홍선영 <allie@hongseon-yeong-ui-MacBookPro.local>
2022-05-26 08:49:48 +0900

    refactor: type 수정

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -2,7 +2,11 @@ import ReactLoading from 'react-loading';
 
 import styles from './loading.module.scss';
 
-const Loading = (loading: any) => {
+interface Props {
+  loading: boolean;
+}
+
+const Loading = (loading: Props) => {
   return (
     <div className={styles.loadingWrapper}>
       {loading && <ReactLoading type='spin' color='#007be9' height={300} width={300} />}
