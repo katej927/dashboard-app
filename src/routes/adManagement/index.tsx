a28c0de69f5ab027c9c677795fa2f1c953498118
daseuls <dls2tmfs2@naver.com>
2022-05-25 17:53:56 +0900

    feat: origin main pull

--- src/routes/adManagement/index.tsx
+++ src/routes/adManagement/index.tsx
@@ -10,7 +10,7 @@ const AdManagement = () => {
       <WhiteSection>
         <div className={styles.wrapper}>
           <section className={styles.buttonWrapper}>
-            <div>전체 광고</div>
+            <div className={styles.adDropDown}>전체 광고</div>
             <button type='button'>광고 만들기</button>
           </section>
           <section className={styles.adManagementWrapper}>
