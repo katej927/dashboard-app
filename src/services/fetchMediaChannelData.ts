d79c626e936a6c13f3b7faf86a85adc8465f0bdb
solchan98 <dev.sol0427@gmail.com>
2022-06-13 21:34:14 +0900

    chore: 서버 변경으로 인한 BASE_URL 수정

--- src/services/fetchMediaChannelData.ts
+++ src/services/fetchMediaChannelData.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { IPeriod } from 'types/period';
 
-const BASE_URL = 'https://madup.shop/api/v1';
+const BASE_URL = 'https://madup-1a.herokuapp.com/api/v1';
 
 export const fetchMediaChannelData = ({ startDate, endDate }: IPeriod) => {
   return axios.get(`${BASE_URL}/media-report`, {
